59f6bf46d2e648b3f82b8085c894e9a4177fad5f
Agbaye Gabriel <agbaye.gabriel@gmail.com>
2026-03-12 11:05:22 +0100

    Fix comment formatting in Question.go

--- Question.go
+++ Question.go
@@ -1,5 +1,4 @@
-//Write a function that takes a slice of words and applies uppercase to
-the last N words
+//Write a function that takes a slice of words and applies uppercase to the last N words
 // words = ["this", "is", "so", "exciting"], n = 2
 // -> ["this", "is", "SO", "EXCITING"]
 
